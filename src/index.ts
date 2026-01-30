c9cd30a254d6a2bb8d5d44df2ffba5899c72a2cf
cristiamBC <109766780+cristiamBC@users.noreply.github.com>
2026-01-29 20:14:35 -0500

    Update index.ts
    backend v1.1

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,3 @@
-#import { renderHtml } from "./renderHtml";
-#
-#export default {
-#	async fetch(request, env) {
-#		const stmt = env.DB.prepare("SELECT * FROM comments LIMIT 3");
-#		const { results } = await stmt.all();
-#
-#		return new Response(renderHtml(JSON.stringify(results, null, 2)), {
-#			headers: {
-#				"content-type": "text/html",
-#			},
-#		});
-#	},
 export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url);
